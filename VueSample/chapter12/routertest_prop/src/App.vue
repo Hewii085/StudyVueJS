5b33b19bd29604b538e94f088381d1dccc5634a5
bin <kurai085@gmail.com>
2020-01-12 17:13:02 +0900

    .

---
+++ VueSample/chapter12/routertest_prop/src/App.vue
@@ -0,0 +1,67 @@
+
+<template>
+  <div>
+    <div class="header">
+      <h1 class="headerText">OPENSG</h1>
+      <nav>
+        <ul>
+          <li><router-link v-bind:to="{name :'home'}">HOME</router-link></li>
+          <li><router-link v-bind:to="{name :'about'}">About</router-link></li>
+          <li><router-link v-bind:to="{name :'contacts'}">Contacts</router-link></li>
+        </ul>
+      </nav>
+    </div>
+
+    <div class="contaier">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue';
+import About from './components/About.vue';
+import Contacts from './components/Contacts.vue';
+import ContactByNo from './components/ContactByNo.vue';
+import NotFound from './components/NotFound.vue';
+import VueRouter from 'vue-router'
+
+const router = new VueRouter({
+  mode : 'history',
+  routes : [
+    { path:'/', component: Home },
+    { path:'/home',name:'home', component: Home },
+    { path:'/about',name:'about', component: About },
+    { path:'/contacts',name:'contacts', component: Contacts, 
+      children : [
+        { path : ':no',name :'contactbyno', component : ContactByNo, props:true}
+        ]
+
+    },
+    {path :'*', component: NotFound}
+  ]
+})
+
+export default {
+  name : 'app',
+  router
+}
+</script>
+
+<style>
+.header { background-color:aqua; padding: 10px 0px 0px 0px; }
+.headerText { padding: 0px 20px 0px 20px; } 
+ul { list-style-type: none; margin: 0; padding: 0;
+    overflow: hidden; background-color: purple;  }
+li { float: left; }
+li a { display: block; color: yellow; text-align: center;
+    padding: 14px 16px; text-decoration: none;  }
+li a:hover { background-color: aqua; color:black; }#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ VueSample/chapter12/routertest_prop/src/components/ContactByNo.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <h1 class="divider"></h1>
+        <div>
+            <table class="detail table table-bordered">
+                <tbody>
+                    <tr class="active">
+                        <td>serial</td>
+                        <td>{{contact.no}}</td>
+                    </tr>
+                    <tr class="active">
+                        <td>Name</td>
+                        <td>{{contact.name}}</td>
+                    </tr>
+                    <tr class="active">
+                        <td>Tel</td>
+                        <td>{{contact.tel}}</td>
+                    </tr>
+                    <tr class="active">
+                        <td>Address</td>
+                        <td>{{contact.address}}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+<script>
+import contactlist from '../ContactList'
+
+export default {
+    name : 'contactbyno',
+    props : ['no'],
+    data:function() {
+        return {
+            contacts:contactlist.contacts
+        }
+    },
+    computed:{
+        contact : function(){
+            var no = this.no;
+            var arr = this.contacts.filter(function(item){
+                return item.no == no;
+            });
+            if(arr.length == 1)
+                return arr[0];
+            else
+                return {};
+        }
+    },
+}
+</script>
+
+<style>
+table.detail { width:400px; }
+.divider {height : 3px; margin-left: auto; margin-right : auto;
+          background-color:#FF0066; color:#FF0066; border: 0 none;}
+</style>
